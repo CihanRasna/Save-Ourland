53af415c0b1508da8fbb18c70ce066350aebf531
Cihan Rasna <cihanrasna@gmail.com>
2020-11-04 01:27:18 +0300

    Environment and opponents added. Anims started.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    private int _currentSceneIndex;
+    [SerializeField] private float waitTime = 3f;
+
+    private void Start()
+    {
+        _currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (_currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadStartScene());
+        }
+    }
+    
+    IEnumerator LoadStartScene()
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(_currentSceneIndex + 1);
+    }
+
+    public void LoadGameScreen()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        
+    }
+
+    public void RestartGameScreen()
+    {
+        
+    }
+
+}
